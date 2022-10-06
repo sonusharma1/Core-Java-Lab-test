6cd1238efed622c61f0c8298ff55be9c6744a104
sonusharma1 <90684040+sonusharma1@users.noreply.github.com>
2022-10-06 17:07:18 +0530

    06 Oct 2022

---
+++ StringPalindromeDemo/src/com/demo/StringDemo.java
@@ -0,0 +1,35 @@
+package com.demo;
+
+import java.util.Scanner;
+
+public class StringDemo {
+
+	// method to check string is palindrome or not
+	static void isPalindrome(String inputString) {
+
+		String reverseString = "";
+
+		// reversing inputString and store its value into reverseString
+		for (int i = inputString.length() - 1; i >= 0; i--) {
+			reverseString += inputString.charAt(i);
+		}
+
+		// comparing values of reverseString and inputString
+		if (inputString.equals(reverseString)) {
+			System.out.println(inputString + " is a Palindrome");
+		} else {
+			System.out.println(inputString + " is not a Palindrome");
+		}
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a word");
+
+		// calling method and also taking input from user
+		isPalindrome(sc.next());
+
+	}
+
+}
